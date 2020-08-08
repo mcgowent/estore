8ab8e4149dd1ae464b798c42ea1697db74375cba
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-08 16:05:49 -0700

    Added additional styles and item component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 
 // Components
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
