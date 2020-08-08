4c2cdbad6525ea9e89fd72828b5ef2b22535eb43
Timothy McGowen <mcgowen17@yahoo.com>
2020-08-08 12:59:23 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+// Styles
 import './App.css';
+
+// Components
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path="/" component={HomePage} />
     </div>
   );
 }
